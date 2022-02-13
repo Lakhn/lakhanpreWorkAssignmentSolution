7f7fc4a96f0b139e514d11e034a0babe5575090c
Lakhn <67182457+Lakhn@users.noreply.github.com>
2022-02-13 17:03:06 +0530

    Add files via upload

---
+++ lakhanpreworkassignments/star.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class star
+{
+    public static void main(String[] args)
+    {
+        // Create a new Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the number of rows from the user
+        System.out.println("Enter the number of rows needed to print the pattern ");
+
+        int rows = scanner.nextInt();
+        System.out.println("## Printing the pattern ##");
+        
+        // Print i number of stars
+        for (int i=1; i<=rows; i++)
+        {
+            for (int j=1; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
